dff732db1d3e2de10274c366b55054454dcc512a
LucasCampoli <112361327+LucasCampoli@users.noreply.github.com>
2025-11-29 02:57:39 -0300

    Rename api.ts to axiosClient.ts

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,7 +8,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Eye, EyeOff } from "lucide-react"
-import api from "@/lib/api"
+import api from "@/api/axiosClient"
 
 const loginSchema = z.object({
   email: z
