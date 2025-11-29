a0a5bc5879d1dbef84bd18b5137a2526ff917189
LucasCampoli <112361327+LucasCampoli@users.noreply.github.com>
2025-11-29 01:33:58 -0300

    Add shadcn/ui and improve code

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import react from "@vitejs/plugin-react"
-import * as path from "node:path"
 import { defineConfig } from "vitest/config"
 import packageJson from "./package.json" with { type: "json" }
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     open: true,
   },
